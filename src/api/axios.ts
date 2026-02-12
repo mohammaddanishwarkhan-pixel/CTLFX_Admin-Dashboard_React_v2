9ad52a03a1b46ef2d20f80d3734084ce3d7055d6
Mahmood-Ahmad-75 <mahmoodraj75@gmail.com>
2026-02-13 01:01:30 +0500

    favicon update

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://ctlfx-admin-pannel.onrender.com/api', // Update this to your backend URL
+  // baseURL: 'https://ctlfx-admin-pannel.onrender.com/api', // Update this to your backend URL
+  baseURL: " http://localhost:3000/api"
   // timeout: 10000,
 });
 
